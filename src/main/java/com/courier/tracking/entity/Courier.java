170fcfad5f1efa58475ddddf3a609f66f60af0d5
25200185 <alper.karabay@tokeninc.com>
2025-03-10 13:08:42 +0300

    unique constarint on courier name removed

--- src/main/java/com/courier/tracking/entity/Courier.java
+++ src/main/java/com/courier/tracking/entity/Courier.java
@@ -17,7 +17,7 @@ public class Courier {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
