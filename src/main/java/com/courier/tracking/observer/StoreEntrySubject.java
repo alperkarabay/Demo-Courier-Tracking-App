bb14360dc864b795c7b7f5c842eff367ab0252af
25200185 <alper.karabay@tokeninc.com>
2025-03-10 10:35:21 +0300

    added a default observer at startup

--- src/main/java/com/courier/tracking/observer/StoreEntrySubject.java
+++ src/main/java/com/courier/tracking/observer/StoreEntrySubject.java
@@ -2,13 +2,19 @@ package com.courier.tracking.observer;
 
 import com.courier.tracking.entity.Courier;
 import com.courier.tracking.entity.Store;
+import com.courier.tracking.logger.StoreEntryLogger;
+import jakarta.annotation.PostConstruct;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StoreEntrySubject {
     private final List<StoreEntryObserver> observers = new ArrayList<>();
-
+    StoreEntryLogger storeEntryLogger; // Default Observer
+    @PostConstruct
+    public void init() {
+        addObserver(storeEntryLogger); // Add default observer at startup
+    }
     public void addObserver(StoreEntryObserver observer) {
         observers.add(observer);
     }
